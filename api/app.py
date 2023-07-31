cc91c4911c04d890117ccd132509ab107713fe2f
AlirezaPourchali <arp.joker82@gmail.com>
2023-07-31 20:28:15 +0330

    merged with lp

---
+++ api/app.py
@@ -0,0 +1,43 @@
+import mysql.connector
+from datetime import date, datetime, timedelta
+mydb = mysql.connector.connect(host="localhost" , user="root" , password="admin123")
+
+cursor = mydb.cursor()
+
+DB_NAME = 'mydatabase'
+TABLES = {}
+TABLES['employees'] = (
+    "CREATE TABLE `employees` ("
+    "  `emp_no` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `birth_date` date NOT NULL,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `gender` enum('M','F') NOT NULL,"
+    "  `hire_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`)"
+    ") ENGINE=InnoDB")
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+        
+cursor.execute("SHOW DATABASES")
+
+for x in cursor:
+    print (x)
